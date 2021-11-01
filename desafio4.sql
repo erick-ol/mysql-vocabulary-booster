9d957a39258c5b5ecb504fb422e3a5d64084bb9c
Erick Santos <erickosantos.dev@gmail.com>
2021-11-01 13:58:33 -0300

    desafio 5

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT 
   J.JOB_TITLE AS 'Cargo',
-  ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
+  ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
   CASE 
     WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -13,4 +13,4 @@ FROM
   hr.employees AS E
   ON J.JOB_ID = E.JOB_ID
 GROUP BY J.JOB_ID
-ORDER BY `Média salarial`, J.JOB_TITLE;
+ORDER BY 'Média salarial', J.JOB_TITLE;
