2d09a20e049df4931ba102f4de626fef506cee6c
Erick Santos <erickosantos.dev@gmail.com>
2021-11-01 17:27:00 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,17 @@
+SELECT 
+  t1.ContactName AS 'Nome',
+  t1.Country AS 'País',
+  (
+    SELECT COUNT(t2.Country)-1 
+    FROM w3schools.customers AS t2
+    WHERE t1.Country = t2.Country
+  ) AS 'Número de compatriotas'
+FROM 
+w3schools.customers AS t1
+HAVING 
+  (
+    SELECT COUNT(t2.Country)-1 
+    FROM w3schools.customers AS t2
+    WHERE t1.Country = t2.Country
+  ) > 0
+ORDER BY t1.ContactName ;
